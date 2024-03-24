e7af8c7fce5da25b6056bbeec5a09cf282c578b5
Daniel Danner <daniel@danner.de>
2024-03-24 22:54:42 +0100

    Add savings scenarios showing report inaccuracies
    Most return rates are calculated correctly, but there's issues with
    interleaved gains (caused by gains interpolation in analysis) and 100%
    rates (cause unknown).
    
    Also add helper lines to mass-bump snapshots.

--- tests/test_ltfa.py
+++ tests/test_ltfa.py
@@ -3,6 +3,7 @@ import testfixtures
 import pytest
 from pathlib import Path
 import os
+import shutil
 
 import ltfa
 import ltfa.util
@@ -15,17 +16,21 @@ import ltfa.util
         'savings_varying_interest',
         'shared_ownership',
         'simple_checking',
-        'simple_savings',
+        'simple_savings_100p',
+        'simple_savings_5p_payout_dec31',
+        'simple_savings_5p_payout_feb28',
+        'simple_savings_5p_payout_jan1',
+        'simple_savings_5p_payout_jan2',
+        'simple_savings_5p_payout_jan2_noleapyears',
+        'simple_savings_5p_payout_march31',
+        'simple_savings_5p_payout_march31_noleapyears',
+        'simple_savings_5p_with_noise',
         'simple_savings_monthly',
      ),
 )
 def test_real_data_snapshot(tmpdir, request, scenario):
     scenario_dir = Path(__file__).parent / 'scenarios' / scenario
     assert scenario_dir.is_dir()
-
-    with open(scenario_dir / f'{scenario}.log', "r") as fh:
-        # Transform log into format that matches LogCapture:
-        expected_log = [("root", *l.rstrip().split(": ", 1)) for l in fh.readlines()]
 
     expected_investment_report = scenario_dir / f'{scenario}.investment_report.txt'
 
@@ -43,9 +48,23 @@ def test_real_data_snapshot(tmpdir, request, scenario):
 
     with testfixtures.LogCapture(level=logging.INFO) as log_capture:
         ltfa.run(args)
+
+        # Uncomment to rewrite log snapshot:
+        #  with open(scenario_dir / f'{scenario}.log', "w") as fh:
+            #  for line in log_capture:
+                #  print(': '.join(line[1:]), file=fh)
+
+        with open(scenario_dir / f'{scenario}.log', "r") as fh:
+            # Transform log into format that matches LogCapture:
+            expected_log = [("root", *l.rstrip().split(": ", 1)) for l in fh.readlines()]
+
         log_capture.check(*expected_log)
 
     assert bokeh_html.exists()
+
+    #  Uncomment to rewrite report snapshot:
+    #  if investment_report.exists():
+        #  shutil.copyfile(investment_report, scenario_dir / f'{scenario}.investment_report.txt', )
 
     if expected_investment_report.exists():
         with open(scenario_dir / f'{scenario}.investment_report.txt', "r") as left:
